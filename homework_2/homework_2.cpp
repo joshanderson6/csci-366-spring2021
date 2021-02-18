fe34c2511709833038405541c8f14be44d096dfd
joshanderson6 <josh.andersonn94@gmail.com>
2021-02-17 20:21:12 -0700

    homework 2 complete

--- homework_2/homework_2.cpp
+++ homework_2/homework_2.cpp
@@ -4,6 +4,20 @@ char * print_binary_representation(unsigned int i, char *buffer){
     buffer[0] = '0';
     buffer[1] = 'b';
     // fill out remaining 32 bits, 1 or 0 depending on the value in the number i
+
+    int index = 34; // size of buffer is 32 + 2 since '0b' were added initially
+
+    while(i > 0 && index > 1) {  // using num / 2, the remainders get us the binary values in reverse order
+        buffer[--index] =  (i % 2u) + '0'; // added the + '0' and '\0' to keep C from cutting off end values
+        buffer[strlen(buffer)] = '\0';
+        i /= 2u;
+    }
+
+    for (int j = 2; j < 34; j++) { // adds 0's where there are no 1's
+        if (buffer[j] != '1') {
+            buffer[j] = '0';
+        }
+    }
     return buffer;
 }
 
@@ -13,6 +27,7 @@ char * print_binary_representation(unsigned int i, char *buffer){
  *
  * The test below show what the expected values are for given inputs
  */
+
 TEST(print_binary_representation, works) {
     // row 1
     char buffer[50] = {0}; // init to 0
@@ -37,14 +52,14 @@ struct Person {
     int age;
 };
 
-void set_my_age(struct Person p) {
-    p.age = 44;
+void set_my_age(struct Person *p) {
+    p -> age = 44; // Changed p to be a pointer in order to reference a person stored somewhere else in memory
 }
 
 int get_my_age() {
     struct Person me;
     me.name = "Carson";
-    set_my_age(me);
+    set_my_age(&me); // Inputting just "me" was sending in the pointer versus "&me" which sends in the address of the pointer
     return me.age;
 }
 
